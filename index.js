649c6d4099ba49f36f9a7613d47eb2048bbd7faf
dinsin <deensummer99@gmail.com>
2024-03-19 15:32:20 +0800

    BingooOoo

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
 console.log("Hello World!");
+let B = false, 
+I = false,
+N = false,
+G = false,
+O = false;
 
 generateNumber(75);
 
 function generateNumber(max) {
-   // Add = +
-   // Sub = -
-   // Div = /
-   // Mult = *
-   // Rem = %
+// Return random number 0 to 10;
    let randomNumber = Math.floor(Math.random() * max) + 1;
 
-  // if ...else if...else statement
-  //if (condition) {block of code to execute}
-  // =, !, =, >, <, > =, < =, &&, ||
+// if ...else if...else statement
+//if (condition) {block of code to execute}
+// =, !, =, >, <, > =, < =, &&, ||
 
    if (randomNumber <= 15) {
-    return console.log(`Random Number ${randomNumber} is in B. 1 - 15`);
+      B = true;
+      console.log(`Random Number ${randomNumber} is in B. 1 - 15`);
       
    } else if (randomNumber >= 16 && randomNumber <= 30) {
-      return console.log(`Random Number ${randomNumber} is in I. 16 - 30`);
+      I = true;
+      console.log(`Random Number ${randomNumber} is in I. 16 - 30`);
       
    } else if (randomNumber >= 31 && randomNumber <= 45) {
-    return console.log(`Random Number ${randomNumber} is in N. 31 - 45`);
+      N = true;
+      console.log(`Random Number ${randomNumber} is in N. 31 - 45`);
    }
-   else if (randomNumber >= 46 && randomNumber <= 55) {
-    return console.log(`Random Number ${randomNumber} is in G. 46 - 55`);
+   else if (randomNumber >= 46 && randomNumber <= 60) {
+      G = true;
+      console.log(`Random Number ${randomNumber} is in G. 46 - 55`);
    }
-   else if (randomNumber >= 56 && randomNumber <= 75) {
-    return console.log(`Random Number ${randomNumber} is in O. 55 - 75`);
+   else if (randomNumber >= 61 && randomNumber <= 75) {
+      O = true;
+      console.log(`Random Number ${randomNumber} is in O. 55 - 75`);
    }
    else {
       console.log('The number is invalid.')
    }
 
-   return console.log(randomNumber);
+if (B == true && I == true && N == true && G == true && O == true) {
+   console.log(`BINGOOOO SI DINSIN!! :D`);
+   }
+
 }
